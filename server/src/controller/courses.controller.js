e4abc6e6dc47ae9603099b737f964d8683b86598
Vo Thanh <tthanhh410@gmail.com>
2021-10-15 12:35:43 +0700

    feat: create search course by name function

--- server/src/controller/courses.controller.js
+++ server/src/controller/courses.controller.js
@@ -1,4 +1,7 @@
-const { getAllCoursesService } = require("../service/courses.services.js");
+const {
+  getAllCoursesService,
+  getAListCoursesByNameService,
+} = require("../service/courses.services.js");
 const getAllCourse = async (req, res) => {
   try {
     const find = await getAllCoursesService();
@@ -12,6 +15,29 @@ const getAllCourse = async (req, res) => {
       message: "Something went wrong",
       data: {},
     });
+  }
+};
+
+const getAListCoursesByName = async (req, res) => {
+  if (!req.params.name) {
+    return res.status(400).json({
+      message: "Name of category cannot be empty",
+    });
+  } else {
+    try {
+      const result = await getAListCoursesByNameService(req.params.name);
+      return res.status(200).json({
+        success: "Success",
+        data: result,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+        data: {},
+      });
+    }
   }
 };
 const createNewCourse = async (req, res) => {
@@ -29,4 +55,4 @@ const createNewCourse = async (req, res) => {
   }
 };
 
-module.exports = { getAllCourse, createNewCourse };
+module.exports = { getAllCourse, createNewCourse, getAListCoursesByName };
--- server/src/service/courses.services.js
+++ server/src/service/courses.services.js
@@ -1,18 +1,30 @@
-const database = require('../../database/models/index');
+const database = require("../../database/models/index");
 const Course = database.db.Course;
 
-const getAllCoursesService = async () =>{
-    let allCourses = await Course.findAll();
-    return allCourses;
+const getAllCoursesService = async () => {
+  let allCourses = await Course.findAll();
+  return allCourses;
 };
 
-const createNewCourseService = async (body,page,limit) =>{
-    await Course.create({
-        name: body.name,
-        description: body.description
-    });
-}
+const createNewCourseService = async (body) => {
+  await Course.create({
+    name: body.name,
+    description: body.description,
+  });
+  return;
+};
 
+const getAListCoursesByNameService = async (nameToFind) => {
+  let selectedCourse = await Course.findAll({
+    where: {
+      name: nameToFind,
+    },
+  });
+  return selectedCourse;
+};
 
-
-module.exports =  { getAllCoursesService, createNewCourseService };
+module.exports = {
+  getAllCoursesService,
+  createNewCourseService,
+  getAListCoursesByNameService,
+};
