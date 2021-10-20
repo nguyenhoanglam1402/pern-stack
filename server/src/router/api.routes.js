48d5c839b4df8b3fd571d33147a5388dfa6d800b
Vo Thanh <tthanhh410@gmail.com>
2021-10-20 15:41:02 +0700

    update: update the router to call api

--- server/src/router/api.routes.js
+++ server/src/router/api.routes.js
@@ -3,7 +3,7 @@ const api = require("./routes");
 const traineeApi = require("./trainee.routes");
 const apiRoute = express();
 
-apiRoute.use("/api/v1/trainee", traineeApi);
-apiRoute.use("/api/v1/staff", api);
+apiRoute.use("/v1/trainee", traineeApi);
+apiRoute.use("/v1/staff", api);
 
 module.exports = apiRoute;
