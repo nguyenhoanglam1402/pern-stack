02b28f3ece9c21754a367df2a9436d97d3083d2e
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-10-20 16:06:00 +0700

    test

--- server/src/router/api.routes.js
+++ server/src/router/api.routes.js
@@ -3,7 +3,7 @@ const api = require("./routes");
 const traineeApi = require("./trainee.routes");
 const apiRoute = express();
 
-apiRoute.use("/api/v1/trainee", traineeApi);
-apiRoute.use("/api/v1/staff", api);
+apiRoute.use("/trainee", traineeApi);
+apiRoute.use("/staff", api);
 
 module.exports = apiRoute;
