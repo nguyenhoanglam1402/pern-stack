51b799a0349958bb87e64c4d50125e5daba0c15d
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-10-26 11:12:20 +0700

    feat(auth reducer): Added auth reducer for Redux store

--- client/src/components/common/Navigator/index.jsx
+++ client/src/components/common/Navigator/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./styles.css";
 
 const Navigator = () => {
-  return <nav className="navbar"></nav>;
+  return (
+    <nav className="navbar">
+      <ul className="navbar-menu">
+        <li className="navbar-item">Home</li>
+        <li className="navbar-item">Manage</li>
+        <li className="navbar-item">Home</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigator;
