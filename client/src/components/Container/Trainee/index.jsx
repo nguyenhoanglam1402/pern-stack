e6b5dff26d91553f3a621d0b45428e2f83da9a86
Vo Thanh <tthanhh410@gmail.com>
2021-11-04 00:13:16 +0700

    feat: create trainer page

---
+++ client/src/components/Container/Trainee/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TraineeContainer = () => {
+    return (
+        <div className="trainee-container">
+            
+        </div>
+    )
+}
+
+export default TraineeContainer;
